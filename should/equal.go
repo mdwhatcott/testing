481fec0a92e0b14ab4e5437c68205cde322d2edf
Michael Whatcott <mdwhatcott@gmail.com>
2021-06-03 01:02:52 -0600

    Assertion wishlist.

--- should/equal.go
+++ should/equal.go
@@ -116,3 +116,48 @@ var (
 	errActualTypeMismatch   = errors.New("actual type mismatch")
 	errEqualityMismatch     = errors.New("equality mismatch")
 )
+
+/*
+
+## TODO
+
+- Equal for numerics of differing type: So(8, should.Equal, uint(8))
+- BeEmpty
+- HaveLength
+- Contain
+- ContainKey
+- ContainSubstring
+- StartWith
+- EndWith
+- BeIn
+- BeGreaterThan
+- BeGreaterThanOrEqualTo
+- BeLessThan
+- BeLessThanOrEqualTo
+- Panic
+- PanicWith
+- WrapError
+- HappenAfter
+- HappenBefore
+- HappenBetween
+- HappenOnOrAfter
+- HappenOnOrBefore
+- HappenOnOrBetween
+- HappenWithin
+
+## Negations: need to figure out an elegant way (use result of corresponding positive assertion)
+
+- NotBeNil
+- NotBeEmpty
+- NotEqual
+- NotHaveLength
+- NotContain
+- NotContainKey
+- NotContainSubstring
+- NotStartWith
+- NotEndWith
+- NotBeIn
+- NotPanic
+- NotHappenWithin
+
+*/
