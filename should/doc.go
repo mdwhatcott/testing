a385491bde5207f995f9033f2badb1027b9c8897
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-09-07 16:43:52 -0600

    Corrected package name in the live template.

--- should/doc.go
+++ should/doc.go
@@ -31,11 +31,11 @@ For those using an IDE by JetBrains, you may
 find the following "live template" helpful:
 
 	func Test$NAME$Suite(t *testing.T) {
-		suite.Run(&$NAME$Suite{T: suite.New(t)}, suite.Options.UnitTests())
+		should.Run(&$NAME$Suite{T: should.New(t)}, should.Options.UnitTests())
 	}
 
 	type $NAME$Suite struct {
-		*suite.T
+		*should.T
 	}
 
 	func (this *$NAME$Suite) Setup() {
